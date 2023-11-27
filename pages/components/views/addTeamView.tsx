07043e9743d559f8d9c6ca23aa452b2527aaae90
RuanCampello <ruancampello888@gmail.com>
2023-11-27 18:32:39 -0300

    fix: put and team views select

--- pages/components/views/addTeamView.tsx
+++ pages/components/views/addTeamView.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import FormField from '../formField'
-import { Theme, useTheme } from '@mui/material/styles'
 import Box from '@mui/material/Box';
 import OutlinedInput from '@mui/material/OutlinedInput'
 import InputLabel from '@mui/material/InputLabel'
@@ -10,18 +9,41 @@ import Select, { SelectChangeEvent } from '@mui/material/Select'
 import Chip from '@mui/material/Chip';
 import { CheckCircle, XCircle } from '@phosphor-icons/react';
 import ToastComponent from '../toast';
-import { ListItemText } from '@mui/material';
-import { Checkbox } from 'flowbite-react';
+import { Checkbox, ListItemText } from '@mui/material';
 
+interface Employee {
+  id: number
+  cpf: string
+  date: Date
+  function: string
+  name: string
+  personal_number: string
+}
+interface Team {
+  name: string
+  funcionarioList: Employee[]
+}
+export const labelStyle = {
+  color: 'rgb(226,232,240)',
+  background: 'rgb(30,41,59)',
+  padding: '3px'
+}
+export const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: 48 * 4.5 + 8,
+      width: 250}}
+}
 export default function AddTeamView() {
   const [name, setName] = useState(null)
-  const [fList, setFList] = useState([])
+  const [fList, setFList] = useState<Employee[]>([])
   const [status, setStatus] = useState(Number)
   const [data, setData] = useState(null)
   const [dataErrors, setDataErrors] = useState(null)
   const [open, setOpen] = useState(false)
-  const [employees, setEmployees] = useState([])
-  const [teams, setTeams] = useState([])
+  const [employees, setEmployees] = useState<Employee[]>([])
+  const [teams, setTeams] = useState<Team[]>([])
+  const [options, setOptions] = useState<string[]>([])
 
   async function getEmployees() {
     const response = await fetch(`http://localhost:8080/employee/all`, {
@@ -31,18 +53,6 @@ export default function AddTeamView() {
     const data = await response.json()
     setEmployees(data)
   }
-  const setUnemployeesList = () => {
-    teams.map((team) => {
-      if(team['funcionarioList'][0]) {
-        const employeeName = (team['funcionarioList'][0]['name'])
-        employees.map((emp) => {
-          const name = emp['name']
-          if(name === employeeName) employees.splice(name, 1)
-        }) //get only employee without a team
-      }
-    })    
-  }
-  setUnemployeesList()
   async function getTeams() {
     const response = await fetch('http://localhost:8080/team/all', {
       method: 'GET',
@@ -51,22 +61,6 @@ export default function AddTeamView() {
     const data = await response.json()
     setTeams(data)
   }
-  function getStyles(name: string, personName: readonly string[], theme: Theme) {
-    if(personName) {
-        return {
-        fontWeight:
-          personName.indexOf(name) === -1
-            ? theme.typography.fontWeightRegular
-            : theme.typography.fontWeightMedium } }
-          }
-  const MenuProps = {
-    PaperProps: {
-      style: {
-        maxHeight: 48 * 4.5 + 8,
-        width: 250}}
-  }
-  const theme = useTheme()
-
   async function addTeam() {
     const response = await fetch(`http://localhost:8080/team/`, {
       method: 'POST',
@@ -81,46 +75,64 @@ export default function AddTeamView() {
     if(data!['errors'] !== undefined && data) setDataErrors(data['errors'][0]['defaultMessage'])
     if (data) setData(data)
   }
+  const setUnemployeesList = () => {
+    // setting employees without team 
+    const updatedEmployees = [...employees]
+    teams.forEach((team) => {
+      team.funcionarioList.forEach((func) => {
+        const funcName = func.name
+        const funcId = func.id
+        const index = updatedEmployees.findIndex((employee) => employee.name === funcName && employee.id === funcId)
+        if (index !== -1) {
+          updatedEmployees.splice(index, 1)
+        }
+      })
+    }) 
+    setEmployees(updatedEmployees)
+  }
   function handleSubmit(e: any) {
     e.preventDefault()
     addTeam()
     setOpen(true)
   }
-  function setEmployee(name: String) {
-    employees.map((emp) => {
-      if(emp['name'] === name) fList.push(emp)
+  const setEmployeeList = () => {
+    // setting array that will be sent to api post
+    const updatedList: Employee[] = [...fList]
+    employees.forEach((emp) => {
+      updatedList.push({
+        id: emp.id,
+        cpf: emp.cpf,
+        date: emp.date,
+        function: emp.function,
+        name: emp.name,
+        personal_number: emp.personal_number,
+      })
     })
-  }  
-  const [options, setOptions] = useState<string[]>([]);
+    setFList(updatedList) 
+  }
   const handleChange = (event: SelectChangeEvent<typeof options>) => {
     const {
       target: { value },
     } = event;
     setOptions(typeof value === 'string' ? value.split(',') : value)
+    setEmployeeList()
   }
-  const handleSelect = (event: any) => {
-    setEmployee(String(event.target.value))
-  }  
-  const labelStyle = {
-    color: 'rgb(226,232,240)',
-    background: 'rgb(30,41,59)',
-    padding: '3px'
-  }
-  useEffect(()=> {
+  useEffect(() => {
+    // fetch employees and teams
     getEmployees()
     getTeams()
   }, [])
-  console.log(fList) // verify on set employee if its already on it
-  // change the function to search options names/ids in the employees array
-  // so it will be need just to call the func on post
-  
+  useEffect(() => {
+    // update unemployees list when teams or employees change
+    setUnemployeesList()
+  }, [teams, employees])
   return (
     <div>
       <form className='py-16 xl:px-[150px] px-16' onSubmit={handleSubmit}>
         <div className='grid md:grid-cols-2 md:gap-6'>
           <FormField func={setName} dName={'Nome'} isDefault={false}/>
-  <FormControl sx={{ m: 1, width: 300 }}>
-    <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
+  <FormControl className='w-full rounded-lg h-fit'>
+    <InputLabel id="demo-multiple-checkbox-label" style={labelStyle}>Selecione os funcionários da equipe</InputLabel>
     <Select
       labelId="demo-multiple-checkbox-label"
       id="demo-multiple-checkbox"
@@ -128,16 +140,22 @@ export default function AddTeamView() {
       value={options}
       onChange={(e) => {
         handleChange(e)
-        handleSelect(e)
       }}
-      input={<OutlinedInput label="Tag" />}
-      renderValue={(selected) => selected.join(', ')}
+      input={<OutlinedInput label="Selecione os funcionários da equipe" />}
+      className='bg-slate-800 text-slate-600'
+      renderValue={(selected) => (
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.75 }}>
+          {selected.map((value) => (
+            <Chip style={{color: 'white', background: 'rgb(71,85,105)'}} key={value} label={value} />
+          ))}
+        </Box>
+      )}
       MenuProps={MenuProps}
     >
       {employees.map((employee) => (
-        <MenuItem key={employee['id']} value={employee['name']}>
-          <Checkbox checked={options.indexOf(employee['name']) > -1} />
-          <ListItemText primary={employee['name']} />
+        <MenuItem key={employee.id} value={employee.name}>
+          <Checkbox checked={options.indexOf(employee.name) > -1} />
+          <ListItemText primary={employee.name} />
         </MenuItem>
       ))}
     </Select>
--- pages/components/views/putTeamView.tsx
+++ pages/components/views/putTeamView.tsx
@@ -1,26 +1,38 @@
 import { useEffect, useState } from 'react'
 import FormField from '../formField'
-import Box from '@mui/material/Box';
+import Box from '@mui/material/Box'
 import OutlinedInput from '@mui/material/OutlinedInput'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
-import Chip from '@mui/material/Chip';
-import ToastComponent from '../toast';
-import { useRecoilState } from 'recoil';
-import { teamFunct, teamId, teamName } from '@/atoms/updateTeamAtom';
-import { Theme, useTheme } from '@mui/material';
+import Chip from '@mui/material/Chip'
+import { useRecoilState } from 'recoil'
+import { teamFunct, teamId, teamName } from '@/atoms/updateTeamAtom'
+import { Checkbox, ListItemText } from '@mui/material'
+import { MenuProps, labelStyle } from './addTeamView'
 
+interface Employee {
+  id: number
+  cpf: string
+  date: Date
+  function: string
+  name: string
+  personal_number: string
+}
+interface Team {
+  name: string
+  funcionarioList: Employee[]
+}
 export default function PutTeamView() {
   const [data, setData] = useState(null)
   const [dataErrors, setDataErrors] = useState(null)
   const [status, setStatus] = useState(Number)
   const [open, setOpen] = useState(false)
-  const [options, setOptions] = useState([])
-  const [employees, setEmployees] = useState([])
-  const [teams, setTeams] = useState([])
-  const [fList, setFList] = useState([])
+  const [employees, setEmployees] = useState<Employee[]>([])
+  const [teams, setTeams] = useState<Team[]>([])
+  const [options, setOptions] = useState<string[]>([])
+  const [fList, setFList] = useState<Employee[]>([])
 
   const [tId, setTeamId] = useRecoilState(teamId)
   const [tName, setTeamName] = useRecoilState(teamName)
@@ -60,64 +72,74 @@ export default function PutTeamView() {
     const data = await response.json()
     setTeams(data)
   }
-  useEffect(()=> {
-    getEmployees()
-    getTeams()
-  }, [])
   const setUnemployeesList = () => {
-    teams.map((team) => {
-      if(team['funcionarioList'][0]) {
-        const employeeName = (team['funcionarioList'][0]['name'])
-        employees.map((emp) => {
-          tFunc.map((alreadyEmployeed) => {
-            const thatEmployeeName = emp['name']
-            const alreadyEmployeedName = alreadyEmployeed['name']
-            if(thatEmployeeName === employeeName && thatEmployeeName !== alreadyEmployeedName) employees.splice(thatEmployeeName, 1)
-          })
-        }) //get only employee without a team and the employees already on the team
-      }
-    })    
-  }
-  setUnemployeesList()
-  function setEmployee(name: String) {
-    employees.map((emp) => {
-      if(emp['name'] === name) fList.push(emp)
+    // setting employees without team and employees from selected team
+    setEmployees((prevEmployees) => {
+      const updatedEmployees = [...prevEmployees];
+      // remove employees from teams
+      teams.forEach((team) => {
+        team.funcionarioList.forEach((func) => {
+          const funcName = func.name
+          const funcId = func.id
+          let index = updatedEmployees.findIndex((employee) => employee.name === funcName && employee.id === funcId)
+          while (index !== -1) {
+            updatedEmployees.splice(index, 1)
+            // find the next occurrence, if theres any
+            const nextIndex = updatedEmployees.findIndex(
+              (employee) => employee.name === funcName && employee.id === funcId
+            )
+            index = nextIndex
+          }
+        })
+      })
+      // add employees from tFunc array
+      tFunc.forEach((func) => {
+        const funcName = func['name']
+        const funcId = func['id']
+        const existsInUpdatedEmployees = updatedEmployees.some((employee) => employee.name === funcName && employee.id === funcId)
+        if (!existsInUpdatedEmployees) updatedEmployees.push(func)
+      })
+      return updatedEmployees;
     })
+  }
+
+  const setEmployeeList = () => {
+    // setting array that will be sent to api post
+    const updatedList: Employee[] = [...fList]
+    employees.forEach((emp) => {
+      updatedList.push({
+        id: emp.id,
+        cpf: emp.cpf,
+        date: emp.date,
+        function: emp.function,
+        name: emp.name,
+        personal_number: emp.personal_number,
+      })
+    })
+    setFList(updatedList) 
+  }
+  const handleChange = (event: SelectChangeEvent<typeof options>) => {
+    const {
+      target: { value },
+    } = event;
+    setOptions(typeof value === 'string' ? value.split(',') : value)
+    setEmployeeList()
   }
   function handleSubmit(e: any) {
     e.preventDefault()
     putTeam()
     setOpen(true)
   }
-  const handleSelect = (e: any) => {
-    const value = e.target.value[0][0]['name']
-    setOptions(typeof value === 'string' ? value.split(',') : value,)
-  }
-  const handleChange = (e: any) => {
-    const value = e.target.value
-    const name = value[0][0]['name']
-    if(name) setEmployee(String(name))
-  }
-  const labelStyle = {
-    color: 'rgb(226,232,240)',
-    background: 'rgb(30,41,59)',
-    padding: '3px'
-  }
-  const MenuProps = {
-    PaperProps: {
-      style: {
-        maxHeight: 48 * 4.5 + 8,
-        width: 250}}
-  }
-  function getStyles(name: string, personName: readonly string[], theme: Theme) {
-    if(personName) {
-        return {
-        fontWeight:
-          personName.indexOf(name) === -1
-            ? theme.typography.fontWeightRegular
-            : theme.typography.fontWeightMedium } }
-  }
-  const theme = useTheme()
+  useEffect(() => {
+    // fetch employees and teams
+    getEmployees()
+    getTeams()
+  }, [])
+  useEffect(() => {
+    // update unemployees list when teams or employees change
+    setUnemployeesList()
+  }, [teams, employees])
+  const idToNameMap = new Map<number, string>();
   return (
     <div>
       <form className='py-16 xl:px-[150px] px-16' onSubmit={handleSubmit}>
@@ -129,30 +151,34 @@ export default function PutTeamView() {
         labelId='demo-multiple-chip-label'
         id='demo-multiple-chip'
         multiple
-        value={options ? options : []}
+        value={options}
         onChange={(e) => {
           handleChange(e)
-          handleSelect(e)
         }}
         input={<OutlinedInput id='select-multiple-chip' label='Chip' />}
         className='bg-slate-800 text-slate-600'
         renderValue={(selected) => (
-          <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1 }}>
-            {selected.map((value) => (
-              <Chip style={{color: 'white', background: 'rgb(71,85,105)'}} key={value} label={value} />
-            ))}
+          <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.75 }}>
+            {options.map((optionId) => (
+          <Chip
+            key={optionId}
+            style={{ color: 'white', background: 'rgb(71,85,105)' }}
+            label={idToNameMap.get(optionId)}
+          />
+        ))}
+
           </Box>
         )}
         MenuProps={MenuProps}>
-        {employees.map((employee) => (
-          <MenuItem
-            key={employee['id']}
-            value={[employee]}
-            //style={{background: 'rgb(71,85,105)'}}
-            style={getStyles(employee, options, theme)}>
-            {employee['name']}
-          </MenuItem>
-        ))}
+          {employees.map((employee) => {
+          idToNameMap.set(employee.id, employee.name);
+          return (
+            <MenuItem key={employee.id} value={employee.id}>
+              <Checkbox checked={options.includes(employee.id.toString())} />
+              <ListItemText primary={employee.name} />
+            </MenuItem>
+          )
+        })}
       </Select>
     </FormControl>
     </div>
